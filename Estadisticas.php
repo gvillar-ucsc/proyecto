baa3aba0fbb9d4b1d4a0ee8c3738397eea546c05
gvillar-ucsc <gvillar@it.ucsc.cl>
2026-07-03 20:07:08 -0400

    select estadistica resuelto
    resolvi el select de estadistica que generava error

--- Estadisticas.php
+++ Estadisticas.php
@@ -6,7 +6,7 @@ include 'incluir/navbar_accesos.php';
 /* para hacer el mapa de calor hay que agrupar el total de usuarios dentro de una ubicacion con la ubicacion
 es decir hay que hacer un select con 2 tablas
 */
-$query_1 = "SELECT ubic.ramplazar_campo, COUNT(usurs.ramplazar_campo) as total_usuarios FROM remaplzar_tabla_ubicacion ubic join remaplzar_tabla_usuarios usurs on ubic.ramplazar_clave_primaria = usurs.ramplazar_clave_foranea GROUP BY ubic.remaplzar_id_ubicacion ";
+$query_1 = "SELECT ubic.nombre_ubi, COUNT(reg.tipo_reg) as total_usuarios FROM ubicaciones ubic JOIN registro reg ON ubic.id_ubi = reg.id_reg_ubi WHERE reg.tipo_reg = 'ingreso' GROUP BY ubic.nombre_ubi";
 $result =  mysqli_query($conexion, $query_1);
 
 ?>
@@ -29,7 +29,7 @@ $result =  mysqli_query($conexion, $query_1);
                 <?php while ($fila = mysqli_fetch_array($result)) : ?>
 
                     <tr>
-                        <td><?= $fila['remplazar_nombre_ubicacion'] ?></td>
+                        <td><?= $fila['nombre_ubi'] ?></td>
 
                         /*no remplazes total_usuarios porque es el nombre del campo del resultado del select*/
                         <td><?= $fila['total_usuarios'] ?></td>
